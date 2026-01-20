eaf201265c59422cc6266eb2038f0149d26751db
Nathan93705 <99233134+Nathan93705@users.noreply.github.com>
2026-01-20 11:38:02 -0800

    init

--- packages/core/src/block/traits/support.ts
+++ packages/core/src/block/traits/support.ts
@@ -2,60 +2,152 @@ import { BlockFace } from "@serenityjs/protocol";
 
 import { BlockType } from "../identity";
 import { Block } from "../block";
+import { BlockPlacementOptions } from "../..";
 
 import { BlockTrait } from "./trait";
 
 class BlockSupportTrait extends BlockTrait {
-  public static readonly identifier = "support";
-  public static tag = "plant";
-  public static types = ["minecraft:short_grass"];
+  public static readonly identifier: string = "support";
 
   /**
    * The block type that provides support for this block.
-   * If null, any block type can provide support.
+   * If null, any solid block can provide support.
    */
-  private supportBlockType: BlockType | null = null;
+  protected supportBlockType: BlockType | null = null;
 
   /**
-   * The direction in which the block is supported.
+   * The directions from which the block can be supported.
+   * Only one of these directions will actively support the block at a time.
    */
-  private supportDirections: Array<BlockFace> = [];
+  protected allowedSupportDirections: Array<BlockFace> = [
+    BlockFace.Bottom,
+    BlockFace.Top,
+    BlockFace.North,
+    BlockFace.South,
+    BlockFace.East,
+    BlockFace.West
+  ];
+
+  /**
+   * The direction that is currently providing support.
+   * This is set during placement and checked during updates.
+   */
+  protected activeSupportDirection: BlockFace | null = null;
 
   public constructor(block: Block) {
     super(block);
 
     // By default, support is provided from below
-    this.supportDirections.push(BlockFace.Bottom);
+    if (this.allowedSupportDirections.length === 0)
+      this.allowedSupportDirections.push(BlockFace.Bottom);
+  }
+
+  /**
+   * Checks if a block in the given direction provides valid support.
+   * @param direction The direction to check for support.
+   * @returns True if the block in the given direction provides valid support, false otherwise.
+   */
+  protected isValidSupport(direction: BlockFace): boolean {
+    const supportBlock = this.block.face(direction);
+
+    if (!supportBlock) return false;
+
+    // If a specific support block type is required, check for it
+    if (this.supportBlockType !== null)
+      return supportBlock.type === this.supportBlockType;
+
+    // If no specific support block type is defined, any solid block can provide support
+    return supportBlock.isSolid;
+  }
+
+  /**
+   * Finds the first valid support direction from the allowed directions.
+   * @returns The valid support direction or null.
+   */
+  protected findValidSupportDirection(): BlockFace | null {
+    // Check each allowed support direction for validity
+    for (const direction of this.allowedSupportDirections)
+      if (this.isValidSupport(direction)) return direction;
+
+    return null;
+  }
+
+  public onPlace(options: BlockPlacementOptions): boolean | void {
+    // Find a valid support direction
+    this.activeSupportDirection = this.findValidSupportDirection();
+
+    if (this.activeSupportDirection === null)
+      // No valid support found, cancel placement
+      options.cancel = true;
   }
 
   public onUpdate(): void {
-    // Prepare a flag to track if the block is supported
-    let supported = false;
+    // If no active support direction is set, try to find one
+    if (this.activeSupportDirection === null)
+      this.activeSupportDirection = this.findValidSupportDirection();
 
-    // Check if the block is supported from any of the defined directions
-    for (const direction of this.supportDirections) {
-      // Get the block in the support direction
-      const supportBlock = this.block.face(direction);
-
-      // Check if the support block exists and is of the correct type
-      if (supportBlock && supportBlock.type == this.supportBlockType) {
-        // Set the supported flag to true
-        supported = true;
-        break;
-      } else if (
-        supportBlock &&
-        this.supportBlockType === null &&
-        supportBlock.isSolid
-      ) {
-        // If no specific support block type is defined, any solid block can provide support
-        supported = true;
-        break;
-      }
-    }
-
-    // If the block is not supported, destroy it
-    if (!supported) this.block.destroy({ dropLoot: true });
+    // Check if the active support direction is still valid
+    if (
+      this.activeSupportDirection === null ||
+      !this.isValidSupport(this.activeSupportDirection)
+    )
+      // Support was destroyed, destroy this block
+      this.block.destroy({ dropLoot: true });
   }
 }
 
-export { BlockSupportTrait };
+/**
+ * A trait for blocks that can only be supported from below.
+ * Example: Grass blocks can only be placed on top of solid blocks.
+ */
+class FloorBlockSupportTrait extends BlockSupportTrait {
+  public static readonly identifier = "floor_support";
+  public static types = ["minecraft:short_grass"];
+
+  protected allowedSupportDirections = [BlockFace.Bottom];
+}
+
+/**
+ * A trait for blocks that can only be supported from above.
+ * Example: Hanging lanterns can only be placed on the underside of solid blocks.
+ */
+class CeilingBlockSupportTrait extends BlockSupportTrait {
+  public static readonly identifier = "ceiling_support";
+
+  protected allowedSupportDirections = [BlockFace.Top];
+}
+
+/**
+ * A trait for blocks that can only be supported from the sides.
+ * Example: Wall-mounted torches can only be placed on the sides of solid blocks.
+ */
+class WallBlockSupportTrait extends BlockSupportTrait {
+  public static readonly identifier = "wall_support";
+  public static types = ["minecraft:ladder"];
+
+  protected allowedSupportDirections = [
+    BlockFace.North,
+    BlockFace.South,
+    BlockFace.East,
+    BlockFace.West
+  ];
+}
+
+/**
+ * A trait for blocks that can be supported from both above and below.
+ * Example: Lanterns that can hang from ceilings or sit on floors.
+ */
+class VerticalBlockSupportTrait extends BlockSupportTrait {
+  public static readonly identifier = "vertical_support";
+  public static types = ["minecraft:lantern"];
+
+  protected allowedSupportDirections = [BlockFace.Top, BlockFace.Bottom];
+}
+
+export {
+  BlockSupportTrait,
+  FloorBlockSupportTrait,
+  CeilingBlockSupportTrait,
+  WallBlockSupportTrait,
+  VerticalBlockSupportTrait
+};
